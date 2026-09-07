84eb65bd77444bc0af0b5b0ce9a40dc35c95da61
bbg3-hub <abdullahgood1279@gmail.com>
2026-09-07 15:17:24 +0530

    Add problem 26: Day of the Week - Read number (1-7) and print corresponding day

---
+++ easy-python-lab/26.py
@@ -0,0 +1,20 @@
+day_num = int(input("Enter day number (1-7): "))
+
+if day_num == 1:
+    day = "Monday"
+elif day_num == 2:
+    day = "Tuesday"
+elif day_num == 3:
+    day = "Wednesday"
+elif day_num == 4:
+    day = "Thursday"
+elif day_num == 5:
+    day = "Friday"
+elif day_num == 6:
+    day = "Saturday"
+elif day_num == 7:
+    day = "Sunday"
+else:
+    day = "Invalid day number"
+
+print(f"Day {day_num}: {day}")
